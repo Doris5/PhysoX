7c627206febb61b2c3cd1cceede49da56bfaa164
Doris5 <rmajchrzak7@o2.pl>
2023-08-05 15:39:33 +0200

    Create File Structure

---
+++ client/src/features/contact/Contact.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function Contact() {
+  return (
+      <Typography variant='h2'>
+          Contact
+      </Typography>
+  );
+}
+
+export default Contact;
---
+++ client/src/features/payment/Payment.jsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+function Payment() {
+  return (
+      <Typography variant='h2'>
+          Payment
+      </Typography>
+  );
+}
+
+export default Payment;
